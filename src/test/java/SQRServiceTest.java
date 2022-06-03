5de0b45dd74eb64e84b8e4c29cad740772cbe140
Александр Рязанцев <amapkymap@gmail.com>
2022-06-04 00:18:43 +0300

    Second commit

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,86 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.javaqa.javaqamvn.services.RangeService;
 
 
 public class SQRServiceTest {
 
-    @Test
-    public void shouldCalcExactRange() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/amountSQR.csv")
+    public void shouldCalcExactRange(int expected, int rangeMin, int rangeMax) {
         RangeService rangeService = new RangeService();
 
-        int expected = 6;
-        int actual = rangeService.calcRange(576,873);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactBegin1() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 20;
-        int actual = rangeService.calcRange(1,873);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactBegin99() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 20;
-        int actual = rangeService.calcRange(99,873);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactBegin100() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 20;
-        int actual = rangeService.calcRange(100,873);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactEnd9800() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 89;
-        int actual = rangeService.calcRange(100,9800);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactEnd9801() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 90;
-        int actual = rangeService.calcRange(100,9801);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactEnd9802() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 90;
-        int actual = rangeService.calcRange(100,9802);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcExactEnd100_000() {
-        RangeService rangeService = new RangeService();
-
-        int expected = 90;
-        int actual = rangeService.calcRange(100,100_000);
+        int actual = rangeService.calcRange(rangeMin,rangeMax);
 
         Assertions.assertEquals(expected, actual);
     }
